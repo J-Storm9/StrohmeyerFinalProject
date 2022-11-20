4e9acdb38d9758f41174a11d31b9219d18409e0b
J-Storm9 <jjstrohmeyer9@gmail.com>
2022-11-19 20:02:10 -0600

    started creation of classes

---
+++ StrohmeyerFinalProject/Task.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrohmeyerFinalProject
+{
+    internal class Task
+    {
+
+        /*
+         *This digital task organizer will use a Priority queue to store the task objects.
+         * Users will be able to create, edit, set priorities, and due dates to tasks.
+         * This program will also have functionality for sorting the tasks by priority, due date, and more using a Linked list.
+         *ddd
+         */
+
+        private string taskId;// may not need this
+        private int pri;
+        private DateTime dueDate;
+        private string title;
+        //private string description;
+        private DateTime dateCreated;
+
+        // getters and setters 
+        public string TaskId
+        {
+            get => taskId;
+            set => taskId = value;
+        }
+
+        public int Pri
+        {
+            get => pri;
+            set => pri = value;
+        }
+
+        public DateTime DueDate
+        {
+            get => dueDate;
+            set => dueDate = value;
+        }
+
+        public string Title
+        {
+            get => title;
+            set => title = value;
+        }
+
+        public DateTime DateCreated
+        {
+            get => dateCreated;
+            set => dateCreated = value;
+        }
+
+        //default constructor
+        public Task()
+        {
+
+        }
+    }
+}
---
+++ StrohmeyerFinalProject/TaskList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrohmeyerFinalProject
+{
+    public class TaskList
+    {
+        /*
+         
+         
+         * This program will also have functionality for sorting the tasks by priority, due date, and more using a Linked list.
+         */
+
+        //todo: priority queue to store tasks
+        //todo: method create
+
+        //todo: method edit
+
+        //todo: method set priority
+
+        //todo: method edit due date
+
+
+        //todo: sort by pri
+
+        //todo: sort by due date
+
+        //todo: sort by created
+    }
+}
